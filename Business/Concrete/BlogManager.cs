4d98cf00e877ac26222fe9cc9096f3682da702db
gunelibrahimova <gunel.ibrahimova.2001@gmail.com>
2022-03-26 15:16:32 +0400

    26.03.2022

---
+++ Business/Concrete/BlogManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Business.Concrete
+{
+    public class BlogManager : IBlogManager
+    {
+        private readonly BlogDbContext _context;
+
+        public BlogManager(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(Blog blog)
+        {
+            blog.UpdateDate = DateTime.Now;
+            blog.PublishDate = DateTime.Now;
+            _context.Blogs.Add(blog);
+            _context.SaveChanges();
+        }
+
+        public List<Blog> GetAll()
+        {
+            var blogs = _context.Blogs.Include(x=>x.Category).Include(x=>x.K205User).ToList();
+            return blogs;
+        }
+
+        public Blog GetById(int? id)
+        {
+            var blog = _context.Blogs.FirstOrDefault(x=>x.ID == id.Value);
+            blog.Hit += 1;
+            Update(blog);
+            return blog;
+        }
+
+        public void Update(Blog blog)
+        {
+           _context.Blogs.Update(blog);
+            _context.SaveChanges();
+        }
+    }
+}
